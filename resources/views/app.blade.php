ff83a5c9f506263e4624e88625dae3850b79df91
Cnagnn <yogapratamaputrar@gmail.com>
2026-05-18 17:23:39 +0700

    Switch typography to Inter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,6 +35,12 @@
         <link rel="icon" href="/images/Logo/Logo.svg" type="image/svg+xml">
         <link rel="alternate icon" href="/images/Logo/Logo.svg">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+        >
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
