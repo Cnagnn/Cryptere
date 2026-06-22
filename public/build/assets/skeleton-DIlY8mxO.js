8bf614a2325600f6f9eedc034559318605db03cf
Cnagnn <yogapratamaputrar@gmail.com>
2026-06-22 20:33:16 +0700

    build: production build dengan AppLogo component

--- public/build/assets/skeleton-DIlY8mxO.js
+++
@@ -1,1 +0,0 @@
-import{aJ as a,ai as s}from"./app-BQK0oHCq.js";function o({className:e,...t}){return a.jsx("div",{"data-slot":"skeleton",className:s("animate-pulse rounded-md bg-muted",e),...t})}export{o as S};
---
+++ public/build/assets/skeleton-X_YfDtJK.js
@@ -0,0 +1,1 @@
+import{aK as a,aj as s}from"./app-CL8Mb5Lm.js";function o({className:e,...t}){return a.jsx("div",{"data-slot":"skeleton",className:s("animate-pulse rounded-md bg-muted",e),...t})}export{o as S};
