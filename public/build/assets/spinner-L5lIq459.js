36d5133e9ad48c0cc8662af8e1c67edebcaf5196
Cnagnn <yogapratamaputrar@gmail.com>
2026-06-23 07:18:10 +0700

    build: production build setelah leaderboard enhancements

---
+++ public/build/assets/spinner-L5lIq459.js
@@ -0,0 +1,1 @@
+import{aK as s,aj as e}from"./app-jZrNe4Be.js";import{a8 as i}from"./ui-vendor-CNoE5whc.js";function t({className:a,...r}){return s.jsx(i,{role:"status","aria-label":"Loading",className:e("size-4 animate-spin",a),...r})}export{t as S};
